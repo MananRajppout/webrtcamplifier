9d4e057cb82a18ba220c5ab42fa12c315753f287
enayetsyl <enayetflweb@gmail.com>
2024-10-07 15:10:17 +0600

    waiting room text alignment fixed, leave button removed, dynamic meeting name added

--- frontend/src/app/participant-waiting-room/[id]/page.jsx
+++ frontend/src/app/participant-waiting-room/[id]/page.jsx
@@ -17,8 +17,18 @@ const page = () => {
   const fullName = searchParams.get("fullName");
   const userRole = searchParams.get("role");
   const [participants, setParticipants] = useState([]);
+  const [meetingDetails, setMeetingDetails] = useState([])
 
-  
+  const getMeetingDetails = async (meetingId) => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/get-single-meeting/${meetingId}`
+      );
+      setMeetingDetails(response?.data?.meetingDetails);
+    } catch (error) {
+      console.error("Error in getting meeting details", error);
+    }
+  };
 
   const getParticipantList = async (meetingId) => {
     try {
@@ -45,6 +55,11 @@ const page = () => {
     }
   };
 
+  // Use effect for getting meeting details
+  useEffect(() => {
+    getMeetingDetails(params.id);
+  }, [params.id]);
+
   useEffect(() => {
     const meetingId = params?.id; // Ensure params id is used correctly
     if (meetingId) {
@@ -65,50 +80,51 @@ const page = () => {
         <ParticipantLeftSideBar />
       </div>
       {/* Main content */}
-      <div className="flex-1 w-full max-h-[100vh] overflow-hidden mb-5">
-        <div className="px-5 py-5 flex flex-col justify-between items-between h-full">
-          <div className="h-1/5">
-            {/* First ------ nav bar */}
-            <div className="flex justify-between items-center pb-2">
-              {/* participant name */}
-              <div className="flex justify-start items-center space-x-2 pb-2">
-                <FaVideo />
-                <p className=" text-custom-gray-3 font-semibold">
-                  Waiting Room
-                </p>
+      <div className="flex-1 w-full max-h-[100vh] overflow-hidden mb-5 flex flex-col">
+  <div className="px-5 py-5 flex flex-col justify-between items-between h-full">
+    <div className="h-1/5">
+      {/* First ------ nav bar */}
+      <div className="flex justify-between items-center pb-2">
+        {/* participant name */}
+        <div className="flex justify-start items-center space-x-2 pb-2">
+          <FaVideo />
+          <p className="text-custom-gray-3 font-semibold">
+            Waiting Room
+          </p>
 
-                <Button
-                  children={`Participant View`}
-                  type="button"
-                  variant={"primary"}
-                  className={`text-white py-1 px-3 rounded-xl text-sm`}
-                />
-              </div>
-              {/* logo */}
-              <Logo />
-            </div>
-
-            {/* Second ---------- name bar */}
-            <div className="flex justify-between items-center pb-4">
-              <HeadingBlue25px children="MEETING 01 - PROJECT NAME" />
-              <Button
-                children="Leave"
-                type="submit"
-                variant="meeting"
-                icon={<IoLogOutSharp />}
-                className=" rounded-lg text-custom-black px-3 py-1"
-              />
-            </div>
-          </div>
-
-          {/*Third ---------- meeting stream */}
-          <div className="flex items-center justify-center w-full min-h-screen bg-white rounded-lg">
-            <h1 className="text-2xl font-bold">
-              Please wait, the meeting host will let you in soon.
-            </h1>
-          </div>
+          <Button
+            children={`Participant View`}
+            type="button"
+            variant={"primary"}
+            className={`text-white py-1 px-3 rounded-xl text-sm`}
+          />
         </div>
+        {/* logo */}
+        <Logo />
       </div>
+
+      {/* Second ---------- name bar */}
+      <div className="flex justify-between items-center pb-4">
+        <HeadingBlue25px children={meetingDetails?.title} />
+        {/* <Button
+          children="Leave"
+          type="submit"
+          variant="meeting"
+          icon={<IoLogOutSharp />}
+          className="rounded-lg text-custom-black px-3 py-1"
+        /> */}
+      </div>
+    </div>
+
+    {/*Third ---------- meeting stream */}
+    <div className="flex-grow flex items-center justify-center w-full bg-white rounded-lg">
+      <h1 className="text-2xl font-bold">
+        Please wait, the meeting host will let you in soon.
+      </h1>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 };
